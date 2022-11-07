ee33305063cbe656f4fb755b50383471bcbd3a80
ManishaLalage <manishalalage91@gmail.com>
2022-11-06 23:51:39 -0600

    completed review, menu and images page and started with Order Online …

--- client/src/components/Restaurant/Reviews.jsx
+++ client/src/components/Restaurant/Reviews.jsx
@@ -9,13 +9,13 @@ const Reviews = () => {
     {
       rating: 3.5,
       isRestaurantReview: false,
-      createdAt: "Fri Oct 4 2022 20:20:34 GMT+0530 (India Standard Time)",
+      createdAt: "Fri Oct 14 2022 20:20:34 GMT+0530 (India Standard Time)",
       reviewText: "Very bad experience.",
     },
     {
       rating: 4.5,
       isRestaurantReview: false,
-      createdAt: "Fri Oct 4 2022 20:19:34 GMT+0530 (India Standard Time)",
+      createdAt: "Fri Oct 14 2022 20:19:34 GMT+0530 (India Standard Time)",
       reviewText: "Very good experience.",
     },
   ]);
@@ -23,7 +23,7 @@ const Reviews = () => {
   return (
     <div className="w-full h-full flex-col md:flex md:flex-row relative gap-5">
       <div className="w-full md:w-8/12 flex flex-col gap-3">
-        <div className="md:hidden mb-4">
+        <div className="md:hidden mb-4 flex flex-col gap-2">
           <AddReviewCard />
         </div>
         {reviews.map((review, index) => (
